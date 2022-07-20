d44e2c1b1f5f0dff12bdf90d2e5e845521712d10
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-19 22:55:45 -0300

    metodos get put post delete categoria

--- mr/src/main/java/com/fatec/mr/repository/CategoriaRepository.java
+++ mr/src/main/java/com/fatec/mr/repository/CategoriaRepository.java
@@ -1,5 +1,12 @@
 package com.fatec.mr.repository;
 
-public interface CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mr.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByNome(String nome);
 }
